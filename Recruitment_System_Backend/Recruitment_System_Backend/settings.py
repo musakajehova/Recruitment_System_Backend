2d797daf61a341842fe16d8c552e53e6bee14b5b
Musakajehova Manyathi <musakajehovalindo@gmail.com>
2025-10-05 05:33:21 +0200

    Add Django REST framework and job_board app to settings; create .gitignore file

--- Recruitment_System_Backend/Recruitment_System_Backend/settings.py
+++ Recruitment_System_Backend/Recruitment_System_Backend/settings.py
@@ -31,6 +31,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    ###############################
+    # Additional Apps
+    'job_board.apps.JobBoardConfig',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'django_filters',
+    ###############################
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -120,3 +127,19 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#########################################################################################
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.IsAdminUser',
+    ],
+
+}
+#########################################################################################
