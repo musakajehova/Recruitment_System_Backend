47fc2f0fef792e3951f66d294376b88ac3140902
Musakajehova Manyathi <musakajehovalindo@gmail.com>
2025-10-12 19:04:01 +0200

    created views for the following: RegisterView LoginView LogoutView JobsListView JobsListCreateView DeleteJobsView PersonListView CountriesListCreateView LocationListCreateView IndustryListCreateView CompanyListCreateView JobTypeListCreatView

--- Recruitment_System_Backend/job_board/urls.py
+++ Recruitment_System_Backend/job_board/urls.py
@@ -1,3 +1,28 @@
+from django.urls import path, include
+from .views import (RegisterView, LoginView, LogoutView, JobsListView, JobsListCreateView, DeleteJobsView, 
+                    PersonListView, CountriesListCreateView, LocationListCreateView, IndustryListCreateView, 
+                    CompanyListCreateView, JobTypeListCreatView)
+
+urlpatteterns= [
+    path( 'register/', RegisterView.as_view(), name='register_view'),
+    path( 'login/', LoginView.as_view(), name='login_view'),
+    path( 'logout/', LogoutView.as_view(), name='logout_view'),
+    path( 'jobs/', JobsListView.as_view(), name='jobs_view'),
+    path( 'jobs/create/', JobsListCreateView.as_view(), name='jobsCreate_view'),
+    path( 'jobs/delete/', DeleteJobsView.as_view(), name='deleteJobs_view'),
+    path( 'person/', PersonListView.as_view(), name='person_view'),
+    path( 'countries/', CountriesListCreateView.as_view(), name='countries_view'),
+    path( 'countries/create/', CountriesListCreateView.as_view(), name='countriesCreate_view'),
+    path( 'location/', LocationListCreateView.as_view(), name='location_view'),
+    path( 'location/create/', LocationListCreateView.as_view(), name='locationCreate_view'),
+    path( 'industry/', IndustryListCreateView.as_view(), name='industry_view'),
+    path( 'industry/create/', IndustryListCreateView.as_view(), name='industryCreate_view'),
+    path( 'company/', CompanyListCreateView.as_view(), name='company_view'),
+    path( 'company/create/', CompanyListCreateView.as_view(), name='companyCreate_view'),
+    path( 'jobtype/', JobTypeListCreatView.as_view(), name='jobType_view'),
+    path( 'jobtype/create/', JobTypeListCreatView.as_view(), name='jobTypeCreate_view'),    
+]
+"""
 RegisterView
 LoginView
 LogoutView
@@ -10,3 +35,4 @@ LocationListCreateView
 IndustryListCreateView
 CompanyListCreateView
 JobTypeListCreatView
+"""
